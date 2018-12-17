58d2bb90bda63405b1210539819ff9c508068fbb
jialee <cee5539@postech.ac.kr>
2018-12-17 13:45:32 +0900

    change mode correct

--- stlMC.py
+++ stlMC.py
@@ -72,9 +72,9 @@ class Model:
 
     def reach(self, bound, goal):
         consts = []
-        consts.append(self.init.substitution(self.combineDict(self.makeSubProp(0), self.makeSubVars(0, '0'))))
+        consts.append(self.init.substitution(self.combineDict(self.makeSubMode(0), self.makeSubVars(0, '0'))))
         consts.append(self.flowConstraints(bound))
-        consts.append(goal.substitution(self.combineDict(self.makeSubProp(bound), self.makeSubVars(bound, 't'))))
+        consts.append(goal.substitution(self.combineDict(self.makeSubMode(bound), self.makeSubVars(bound, 't'))))
         return checkSat(consts)
 
 
@@ -97,10 +97,11 @@ class Model:
 
     def modelConstraints(self, bound, timeBound, partition, partitionConsts, formula):
         result = []
-        combine = self.combineDict(self.makeSubProp(0), self.makeSubVars(0, '0'))
+        combine = self.combineDict(self.makeSubMode(0), self.makeSubVars(0, '0'))
         result.append(self.init.substitution(combine))
-       
         result.append(self.flowConstraints(bound))
+        for i in range(len(result)):
+            print(result[i])
 
         ts = [Real("tau_%s"%i) for i in range(0, bound+1)]
 
@@ -132,17 +133,16 @@ class Model:
             result[i] = dict2[i]
         return result
 
+    #Substitution proposition and mode variables according to bound k: {'fonepo' : fonepo_k} 
     def makeSubMode(self, k):
         op = {Type.Bool: Bool, Type.Real: Real, Type.Int: Int}
         subDict = {}
-        for i in self.prop.keys():
-            subDict[str(i.id)] = op[i.getType()](str(i.id) + '_' + str(k))
-        return subDict
-
-    #Substitution proposition according to bound k: {'fonepo' : fonepo_k} 
-    def makeSubProp(self, k):
-        op = {Type.Bool: Bool, Type.Real: Real, Type.Int: Int}
-        subDict = {}
+        for i in set(self.flow.keys()):
+             for j in range(len(i.children)):
+                 mode = i.children[j]
+                 if not(isinstance(mode, Not)):
+                     if (mode.getType() in op.keys()) and (not(str(mode.id) in subDict.keys())):
+                         subDict[str(mode.id)] = op[mode.getType()](str(mode.id) + '_' + str(k))
         for i in self.prop.keys():
             subDict[str(i.id)] = op[i.getType()](str(i.id) + '_' + str(k))
         return subDict
@@ -175,16 +175,16 @@ class Model:
         flowConsts = []
         for k in range(bound):
             time = Real('time' + str(k))
-            combineSub = self.combineDict(self.makeSubProp(k), self.makeSubVars(k, 't'))
-            nextSub = self.combineDict(self.makeSubProp(k+1), self.makeSubVars(k+1, '0'))
+            combineSub = self.combineDict(self.makeSubMode(k), self.makeSubVars(k, 't'))
+            nextSub = self.combineDict(self.makeSubMode(k+1), self.makeSubVars(k+1, '0'))
             const = [And(i.substitution(combineSub), self.jump[i].substitution(combineSub)) for i in self.jump.keys()]
             result = [i.nextSub(nextSub) for i in const]
             combineJump.append(Or(*result))
-            flowConsts.append(Or(*([And(i.substitution(self.makeSubProp(k)), Integral(self.makeSubVars(k, 't'), self.makeSubVars(k, '0'), time, i.children[1], self.flowDictionary(self.flow[i])), Forall(self.flowDictionary(self.flow[i]), time, self.inv[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubProp(k))) for i in self.flow.keys()])))
+            flowConsts.append(Or(*([And(i.substitution(self.makeSubMode(k)), Integral(self.makeSubVars(k, 't'), self.makeSubVars(k, '0'), time, i.children[1], self.flowDictionary(self.flow[i])), Forall(self.flowDictionary(self.flow[i]), time, self.inv[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubMode(k))) for i in self.flow.keys()])))
 
         k = bound
         time = Real('time' + str(bound))
-        flowConsts.append(Or(*([And(i.substitution(self.makeSubProp(k)), Integral(self.makeSubVars(k, 't'), self.makeSubVars(k, '0'), time, i.children[1], self.flowDictionary(self.flow[i])), Forall(self.flowDictionary(self.flow[i]), time, self.inv[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubProp(k))) for i in self.flow.keys()])))
+        flowConsts.append(Or(*([And(i.substitution(self.makeSubMode(k)), Integral(self.makeSubVars(k, 't'), self.makeSubVars(k, '0'), time, i.children[1], self.flowDictionary(self.flow[i])), Forall(self.flowDictionary(self.flow[i]), time, self.inv[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubMode(k))) for i in self.flow.keys()])))
 
         return And(And(*combineJump), And(*flowConsts))
 
@@ -194,8 +194,8 @@ class Model:
         for i in self.prop.keys():
             if str(i) in propSet:
                 for j in self.flow.keys():
-                    const.append(Implies(And(i, j).substitution(self.makeSubProp(k)), Forall(self.flowDictionary(self.flow[j]), time, self.prop[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubProp(k))))
-                    const.append(Implies(And(Not(i), j).substitution(self.makeSubProp(k)), Forall(self.flowDictionary(self.flow[j]), time, Not(self.prop[i]), self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubProp(k))))
+                    const.append(Implies(And(i, j).substitution(self.makeSubMode(k)), Forall(self.flowDictionary(self.flow[j]), time, self.prop[i], self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubMode(k))))
+                    const.append(Implies(And(Not(i), j).substitution(self.makeSubMode(k)), Forall(self.flowDictionary(self.flow[j]), time, Not(self.prop[i]), self.makeSubVars(k, '0'), self.makeSubVars(k, 't'), self.makeSubMode(k))))
         return const
   
 
